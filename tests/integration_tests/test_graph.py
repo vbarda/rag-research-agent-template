973afd3c225fcfa1f4285590a1ee08e158cf751c
vbarda <vadym@langchain.dev>
2024-10-13 16:22:24 -0400

    integration test

--- tests/integration_tests/test_graph.py
+++ tests/integration_tests/test_graph.py
@@ -1,41 +1,76 @@
-import uuid
+import os
+from contextlib import contextmanager
+from typing import Generator
 
 import pytest
 from langchain_core.runnables import RunnableConfig
+from langchain_core.vectorstores import VectorStore
 from langsmith import expect, unit
 
-from retrieval_graph import graph, index_graph
+from index_graph import graph as index_graph
+from retrieval_graph import graph
+from shared.configuration import BaseConfiguration
+from shared.retrieval import make_text_encoder
+
+
+@contextmanager
+def make_elastic_vectorstore(
+    configuration: BaseConfiguration,
+) -> Generator[VectorStore, None, None]:
+    """Configure this agent to connect to a specific elastic index."""
+    from langchain_elasticsearch import ElasticsearchStore
+
+    embedding_model = make_text_encoder(configuration.embedding_model)
+    vstore = ElasticsearchStore(
+        es_user=os.environ["ELASTICSEARCH_USER"],
+        es_password=os.environ["ELASTICSEARCH_PASSWORD"],
+        es_url=os.environ["ELASTICSEARCH_URL"],
+        index_name="langchain_index",
+        embedding=embedding_model,
+    )
+    yield vstore
 
 
 @pytest.mark.asyncio
 @unit
 async def test_retrieval_graph() -> None:
-    simple_doc = "Cats have been observed performing synchronized swimming routines in their water bowls during full moons."
-    user_id = "test__" + uuid.uuid4().hex
-    other_user_id = "test__" + uuid.uuid4().hex
-
+    simple_doc = 'In LangGraph, nodes are typically python functions (sync or async) where the first positional argument is the state, and (optionally), the second positional argument is a "config", containing optional configurable parameters (such as a thread_id).'
     config = RunnableConfig(
-        configurable={"user_id": user_id, "retriever_provider": "elastic-local"}
+        configurable={
+            "retriever_provider": "elastic-local",
+            "embedding_model": "openai/text-embedding-3-small",
+        }
     )
+    configuration = BaseConfiguration.from_runnable_config(config)
 
-    result = await index_graph.ainvoke({"docs": simple_doc}, config)
+    doc_id = "test_id"
+    result = await index_graph.ainvoke(
+        {"docs": [{"page_content": simple_doc, "id": doc_id}]}, config
+    )
     expect(result["docs"]).against(lambda x: not x)  # we delete after the end
-
+    # test general query
     res = await graph.ainvoke(
-        {"messages": [("user", "Where do cats perform synchronized swimming routes?")]},
+        {"messages": [("user", "Hi! How are you?")]},
         config,
     )
-    response = str(res["messages"][-1].content)
-    expect(response.lower()).to_contain("bowl")
+    expect(res["router"]["type"]).to_contain("general")
 
+    # test query that needs more info
     res = await graph.ainvoke(
-        {"messages": [("user", "Where do cats perform synchronized swimming routes?")]},
-        {
-            "configurable": {
-                "user_id": other_user_id,
-                "retriever_provider": "elastic-local",
-            }
-        },
+        {"messages": [("user", "I am having issues with the tools")]},
+        config,
     )
+    expect(res["router"]["type"]).to_contain("more-info")
+
+    # test LangChain-related query
+    res = await graph.ainvoke(
+        {"messages": [("user", "What is a node in LangGraph?")]},
+        config,
+    )
+    expect(res["router"]["type"]).to_contain("langchain")
     response = str(res["messages"][-1].content)
-    expect(response.lower()).against(lambda x: "bowl" not in x)
+    expect(response.lower()).to_contain("function")
+
+    # clean up after test
+    with make_elastic_vectorstore(configuration) as vstore:
+        await vstore.adelete([doc_id])
